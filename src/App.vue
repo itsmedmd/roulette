d45012b7d95100b89f1991f79e90b462c85a306d
Deimantas Butėnas <itsmedmd1@gmail.com>
2022-05-10 13:02:07 +0300

    1 adding and styling URLInputField component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,30 @@
 <template>
-  <div></div>
+    <div class="app">
+        <URLInputField v-model:url="baseURL"/>
+    </div>
 </template>
 
 <script>
+import { ref, watch } from "vue";
+import URLInputField from "./components/URLInputField.vue";
+
 export default {
-  name: 'App'
-}
+    name: "App",
+    components: {
+        URLInputField
+    },
+    setup() {
+        const baseURL = ref("https://dev-games-backend.advbet.com/v1/ab-roulette/1");
+
+        watch(() => baseURL.value, () => {
+            console.log(baseURL.value);
+        });
+
+        return {
+            baseURL
+        };
+    }
+};
 </script>
 
-<style lang="scss"></style>
+<style lang="scss" scoped src="@/assets/styles/components/app.scss"></style>
---
+++ src/components/URLInputField.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="url-field">
+        <label for="url-input" class="url-field__label">Base API URL:</label>
+        <br>
+        <input
+            id="url-input"
+            type="text"
+            class="url-field__input"
+            :value="url"
+            @input="handleInputChange"
+        >
+    </div>
+</template>
+
+<script>
+export default {
+    name: "URLInputField",
+    props: [
+        "url"
+    ],
+    setup(props, {emit}) {
+        const handleInputChange = (event) => {
+            emit("update:url", event.target.value);
+        };
+
+        return {
+            handleInputChange
+        };
+    }
+}
+</script>
+
+<style lang="scss" scoped src="@/assets/styles/components/url-input-field.scss"></style>
