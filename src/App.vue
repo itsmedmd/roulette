c78b6e2de7555637ad5a08581630c77dbff2d3c3
Deimantas Butėnas <itsmedmd1@gmail.com>
2022-05-10 13:36:20 +0300

    4 adding validation for input field url

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,48 @@
 <template>
     <div class="app">
-        <URLInputField v-model:url="baseURL"/>
+        <URLInputField v-model:url="inputURL" />
+        <Statistics :url="currentURL "/>
     </div>
 </template>
 
 <script>
 import { ref, watch } from "vue";
 import URLInputField from "./components/URLInputField.vue";
+import Statistics from "./components/Statistics.vue";
 
 export default {
     name: "App",
     components: {
-        URLInputField
+        URLInputField,
+        Statistics
     },
     setup() {
-        const baseURL = ref("https://dev-games-backend.advbet.com/v1/ab-roulette/1");
+        // default url values
+        const baseURL = "https://dev-games-backend.advbet.com/v1/ab-roulette/";
+        const baseID = 1;
+        const currentURL = ref(baseURL + baseID);
 
-        watch(() => baseURL.value, () => {
-            console.log(baseURL.value);
+        // input url is a separate variable for validation
+        const inputURL = ref(baseURL + baseID);
+
+        watch(() => inputURL.value, () => {
+            // validate if the new URL has the same beginning as `baseURL`
+            const urlStart = inputURL.value.slice(0, baseURL.length);
+            if (urlStart === baseURL) {
+                // split[1] is what comes after `baseURL`
+                const split = inputURL.value.split(baseURL);
+
+                // if split[1] exists, is a number and the new URL is not the same
+                // as the last one, update `currentURL`
+                if (split[1] && !isNaN(split[1]) && inputURL.value !== currentURL.value) {
+                    currentURL.value = inputURL.value;
+                }
+            }
         });
 
         return {
-            baseURL
+            inputURL,
+            currentURL
         };
     }
 };
