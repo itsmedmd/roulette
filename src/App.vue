3672f6bf7c791c88136952585990dd0e71c157b2
Deimantas Butėnas <itsmedmd1@gmail.com>
2022-05-10 13:50:40 +0300

    4 adding wheel configuration retrieval

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
 </template>
 
 <script>
-import { ref, watch } from "vue";
+import { ref, reactive, watch, onMounted } from "vue";
 import URLInputField from "./components/URLInputField.vue";
 import Statistics from "./components/Statistics.vue";
 
@@ -25,6 +25,13 @@ export default {
         // input url is a separate variable for validation
         const inputURL = ref(baseURL + baseID);
 
+        // configuration
+        const configuration = reactive({
+            colors: [],
+            positionToId: [],
+            results: []
+        });
+
         watch(() => inputURL.value, () => {
             // validate if the new URL has the same beginning as `baseURL`
             const urlStart = inputURL.value.slice(0, baseURL.length);
@@ -36,8 +43,33 @@ export default {
                 // as the last one, update `currentURL`
                 if (split[1] && !isNaN(split[1]) && inputURL.value !== currentURL.value) {
                     currentURL.value = inputURL.value;
+
+                    // also update wheel configuration in case it's different
+                    getWheelConfiguration();
                 }
             }
+        });
+
+        const getWheelConfiguration = () => {
+            fetch(currentURL.value + "/configuration")
+            .then((res) => {
+                if (res.status === 200) {
+                    return res.json();
+                }
+                throw new Error("Something went wrong.");
+            })
+            .then((data) => {
+                configuration.colors = data.colors;
+                configuration.positionToId = data.positionToId;
+                configuration.results = data.results;
+            })
+            .catch((err) => {
+                console.error("Configuration fetch failed:", err);
+            });
+        };
+
+        onMounted(() => {
+            getWheelConfiguration();
         });
 
         return {
